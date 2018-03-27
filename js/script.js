6f6c84619a024d380134c4a6950adb97e6d02a60
enmalafeev <malafeev.en@yandex.ru>
2018-03-27 15:49:07 +0300

    main slider done

--- js/script.js
+++ js/script.js
@@ -44,3 +44,31 @@ window.addEventListener("keydown", function(event) {
 	}
 });
 
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slider-item");
+  var dots = document.getElementsByClassName("slider-checker-link");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].className = x[i].className.replace(" active", "");;  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" black", "");
+  }
+  x[slideIndex-1].className += " active";  
+  dots[slideIndex-1].className += " black";
+}
+
